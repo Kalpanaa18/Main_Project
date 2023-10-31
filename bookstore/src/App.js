73b0c79d0cc233d806c9f6da2cd506113456f198
kalpanaa18 <kalpanasharma0118@gmail.com>
2023-10-31 22:18:28 +0530

    added

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -1,11 +1,11 @@
  import './App.css';
-import ContactPage from './ContactPage';
+
 import FirstPage from './FirstPage';
 
 function App() {
   return (
     <div className="App">
-      <ContactPage/>;
+      <FirstPage/>
     </div>
   );
 }
